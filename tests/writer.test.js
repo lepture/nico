63d3856069fcab74b44e649f90b091b8010b4995
Hsiaoming Yang <lepture@me.com>
2012-12-08 17:00:35 +0800

    test post writer

---
+++ tests/writer.test.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+var reader = require('../lib/reader');
+var writer = require('../lib/writer');
+
+var storage = {
+  swigConfig: {
+    root: [path.join(__dirname, 'themes', 'theme1'), path.join(__dirname, 'themes', 'theme2')]
+  },
+  config: {
+    permalink: '{{filename}}.html'
+  },
+  outputDirectory: path.join(__dirname, '_site')
+};
+
+describe('PostWriter', function() {
+  it('should write post design', function() {
+    var post = new reader.Post(
+      path.join(__dirname, 'data', 'design.md'), __dirname
+    );
+    storage.sourcePublicPosts = [post];
+    var p = new writer.PostWriter(storage);
+    console.log();
+    p.start();
+    p.end();
+    var text = fs.readFileSync(path.join(__dirname, '_site', 'design.html'), 'utf8');
+    text.should.equal('Design Pattern');
+  });
+});
+
