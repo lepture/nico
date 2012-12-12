054c4e94fe01001b90a11e96e558ea00e4e1e9db
Hsiaoming Yang <lepture@me.com>
2012-12-12 16:46:43 +0800

    sdk exports

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var reader = require('./lib/reader');
 exports.Post = reader.Post;
+exports.MarkdownParser = reader.MarkdownParser;
 
 var writer = require('./lib/writer');
 for (var key in writer) {
   exports[key] = writer[key];
 }
+
+
+exports.filters = require('./lib/filters');
+exports.utils = require('./lib/utils');
+exports.cli = require('./lib/cli');
+exports.underscore = require('underscore');
