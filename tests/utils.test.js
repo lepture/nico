0253ae29641757b29559c385c661d3f14c593c0f
Hsiaoming Yang <lepture@me.com>
2012-12-14 20:24:18 +0800

    refactor utils

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,29 +1,40 @@
-var require = require('./testutils').require;
-var utils = require('../lib/utils');
 var fs = require('fs');
 var path = require('path');
 var should = require('should');
 var underscore = require('underscore');
+var require = require('./testutils').require;
+var utils = require('../lib/utils');
+var urilab = require('../lib/utils/uri');
+var pathlib = require('../lib/utils/path');
+var Pagination = require('../lib/utils/extra').Pagination;
 
-describe('utils encodeURIComponent', function() {
+
+describe('uri encode', function() {
   it('should be hello-world', function() {
-    utils.encodeURIComponent('Hello World').should.equal('hello-world');
-    utils.encodeURIComponent('H!el?lo-Wo$%rld').should.equal('h-el-lo-wo-rld');
-    utils.encodeURIComponent('`%he()llo<world>').should.equal('he-llo-world');
+    urilab.encode('Hello World').should.equal('hello-world');
+    urilab.encode('H!el?lo-Wo$%rld').should.equal('h-el-lo-wo-rld');
+    urilab.encode('`%he()llo<world>').should.equal('he-llo-world');
   });
   it('can encode unicode', function() {
     var text = '¡å hello 中文';
-    utils.encodeURIComponent(text).should.equal('%C2%A1%C3%A5-hello-%E4%B8%AD%E6%96%87');
+    urilab.encode(text).should.equal('%C2%A1%C3%A5-hello-%E4%B8%AD%E6%96%87');
   });
 });
 
-describe('utils isroot', function() {
+describe('pathlib', function() {
   it('should be root', function() {
-    utils.isroot('/hello').should.equal(true);
+    pathlib.isroot('/hello').should.equal(true);
   });
   it('should not be root', function() {
-    utils.isroot('hello').should.equal(false);
-    utils.isroot('./hello').should.equal(false);
+    pathlib.isroot('hello').should.equal(false);
+    pathlib.isroot('./hello').should.equal(false);
+  });
+  it('should copy docs to _site', function() {
+    var src = path.join(__dirname, '..', 'docs');
+    var dest = path.join(__dirname, '_site');
+    pathlib.copy(src, dest);
+    var isExist = fs.existsSync(path.join(dest, 'index.md'));
+    isExist.should.equal(true);
   });
 });
 
@@ -54,20 +65,11 @@ describe('utils destination', function() {
   });
 });
 
-describe('utils copy', function() {
-  it('should copy docs to _site', function() {
-    var src = path.join(__dirname, '..', 'docs');
-    var dest = path.join(__dirname, '_site');
-    utils.copy(src, dest);
-    var isExist = fs.existsSync(path.join(dest, 'index.md'));
-    isExist.should.equal(true);
-  });
-});
 
 describe('Pagination', function() {
   it('should be a pagination', function() {
     var items = underscore.range(100);
-    var p = new utils.Pagination(items, 2, 30);
+    var p = new Pagination(items, 2, 30);
     p.items.should.eql(underscore.range(30, 60));
     p.total.should.equal(100);
     p.has_prev.should.equal(true);
