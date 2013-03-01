0cf1e4d4089f938abfd29bd88c89fdc6defada20
Hsiaoming Yang <lepture@me.com>
2013-03-01 16:17:27 +0800

    how to export api

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-var reader = require('./lib/reader');
-exports.Post = reader.Post;
-exports.MarkdownParser = reader.MarkdownParser;
-
-var writers = require('./lib/writers');
-Object.keys(writers).forEach(function(key) {
-  exports[key] = writers[key];
-});
-
-
-exports.filters = require('./lib/filters');
-exports.utils = require('./lib/utils');
-exports.cli = require('./lib/cli');
-exports.logging = require('colorful').logging;
+if (process.env.NICO_COVERAGE) {
+  module.exports = require('./lib-cov/');
+} else {
+  module.exports = require('./lib/');
+}
